e9d00fc7c2b946f9fc65d2d7074d70679e3a95f9
Mind Sathirasrindh <109655871+mydjolie@users.noreply.github.com>
2023-08-23 22:42:54 +0700

    fix:fix test-case ex-1

--- tests/ex-1.test.js
+++ tests/ex-1.test.js
@@ -1,16 +1,26 @@
 import fs from "fs/promises";
+import { jest } from "@jest/globals";
 
-describe("exercise 1: Callbacks & Higher Order Functions tests cases", () => {
-  test("ฟังก์ชั่น forEach ต้องเพิ่มเงินเดือนให้กับพนักงานทุกคน 5000 บาท", async () => {
-    const data = await fs.readFile("./ex-1.js");
-    const code = `${data} return newEmployeeSalaries`;
+describe("Callback and Higher order function tests cases", () => {
+  beforeAll(() => {
+    jest.spyOn(console, "log");
+  });
+  afterAll(() => {
+    jest.resetAllMocks();
+  });
 
-    const func = new Function(code);
-    const newEmployeeSalaries = func();
+  describe("exercise 1: Callbacks & Higher Order Functions tests cases", () => {
+    test("ฟังก์ชั่น forEach ต้องเพิ่มเงินเดือนให้กับพนักงานทุกคน 5000 บาท", async () => {
+      const data = await fs.readFile("./ex-1.js");
+      const code = `${data} \n return newEmployeeSalaries`;
 
-    expect(newEmployeeSalaries).toStrictEqual([
-      25005, 45000, 37000, 19500, 349000,
-    ]);
+      const func = new Function(code);
+      const newEmployeeSalaries = func();
+
+      expect(console.log).toHaveBeenCalledWith([
+        25005, 45000, 37000, 19500, 349000,
+      ]);
+      expect(newEmployeeSalaries).toEqual([25005, 45000, 37000, 19500, 349000]);
+    });
   });
 });
-
