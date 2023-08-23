a2d03dbb2b6384200b43982b868b0db187dc9fba
jettapat <jettapat@techupth.com>
2023-08-23 12:11:42 +0700

    fix: starter code

--- ex-2.js
+++ ex-2.js
@@ -4,7 +4,7 @@ function atLeastFive(array, operation) {
   // Start coding here
 }
 
-const studentScoresRoom1 = [12, 40, 67, 80, 100, 15, 40, 78, 75, 80];
+const studentScoresRoom1 = [90, 40, 67, 80, 100, 15, 86, 12, 99, 67];
 const studentScoresRoom2 = [78, 98, 23, 15, 40, 12, 40, 67, 80, 100];
 const studentScoresRoom3 = [67, 80, 100, 15, 12, 40, 67, 80, 100, 67];
 
