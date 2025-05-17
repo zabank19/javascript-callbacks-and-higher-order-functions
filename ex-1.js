7bf5f088a5c64da29a3f66c5c0d78b1c3cabd267
zabank19 <mr.zabank19@gmail.com>
2025-05-17 12:04:49 +0700

    JavaScript Callbacks and Higher Order Functions Assignment

--- ex-1.js
+++ ex-1.js
@@ -2,10 +2,15 @@
 
 function forEach(array, operation) {
   // Start coding here
+  for(let item of array){
+    operation(item);
+  }
 }
 
 const employeeSalaries = [20005, 40000, 32000, 14500, 344000];
 const newEmployeeSalaries = [];
+
+forEach(employeeSalaries,(salary) => newEmployeeSalaries.push(salary + 5000));
 
 // Using `forEach` function here
 
--- ex-2.js
+++ ex-2.js
@@ -2,6 +2,8 @@
 
 function atLeastFive(array, operation) {
   // Start coding here
+  const count = array.filter(operation).length;
+  return count >= 5 ? "✅" : "❌";
 }
 
 const studentScoresRoom1 = [90, 40, 67, 80, 100, 15, 86, 12, 99, 67];
@@ -10,6 +12,10 @@ const studentScoresRoom3 = [67, 80, 100, 15, 12, 40, 67, 80, 100, 67];
 
 // Using `atLeastFive` function here
 
-let scoreRoom1Result;
-let scoreRoom2Result;
-let scoreRoom3Result;
+let scoreRoom1Result = atLeastFive(studentScoresRoom1, (score) => score > 70);
+let scoreRoom2Result = atLeastFive(studentScoresRoom2, (score) => score > 70);
+let scoreRoom3Result = atLeastFive(studentScoresRoom3, (score) => score > 70);
+
+console.log(`นักเรียนห้องที่ 1 ${scoreRoom1Result}`);
+console.log(`นักเรียนห้องที่ 2 ${scoreRoom2Result}`);
+console.log(`นักเรียนห้องที่ 3 ${scoreRoom3Result}`);
